cde9d7f1354b02ecec517f4502d15af40bb5f9de
KarmanGadzza <egor.velikol@gmail.com>
2023-09-02 13:57:47 +0300

    Добавьте файлы проекта.

---
+++ TesrConspect/1_FileS_TD.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	cout << "Size of char : " << sizeof(char) << endl;
+//	cout << "Sizt of int : " << sizeof(int) << endl;
+//	cout << "Size of short int : " << sizeof(short int) << endl;
+//	cout << "Size of long int : " << sizeof(long int) << endl;
+//	cout << "Size of float : " << sizeof(float) << endl;
+//	cout << "Size of double : " << sizeof(double) << endl;
+//	cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+//
+//	return 0;
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//
+//extern int a, b;
+//extern int c;
+//extern float f;
+//
+//int main() {
+//	int a, b;
+//	int c;
+//	float f;
+//
+//	a = 10;
+//	b = 20;
+//	c = a + b;
+//
+//	cout << c << endl;
+//
+//	f = 70.0 / 3.0;
+//	cout << f << endl;
+//
+//	return 0;
+//}
